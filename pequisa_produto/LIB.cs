c415967a33d381757b29eb6d95adae99a9101acf
Guilherme Borsi <Guilherme Borsi@DESKTOP-LHB6NAF>
2022-11-21 00:14:18 -0300

    Adicionar arquivos de projeto.

---
+++ pequisa_produto/LIB.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Projeto_Lib
+{
+    public class Class1
+    {
+        public string Chaveconexao = "Data Source=10.39.45.44;Initial Catalog=TI_Noite;User ID=Turma2022;Password=Turma2022@2022";
+
+        public DataSet Listar_Produtos(string p_Nome_Produto)
+        {
+
+            DataSet DataSetProduto = new DataSet();
+            SqlConnection Conexao = new SqlConnection(Chaveconexao);
+            Conexao.Open();
+
+            string wQuery = $"select * from Produto where Nome_Produto = '{p_Nome_Produto}'";
+            SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
+            adapter.Fill(DataSetProduto);
+
+            foreach (DataRow linha in DataSetProduto.Tables[0].Rows)
+            {
+                Console.WriteLine(linha["Nome_Produto"]);
+            }
+            Conexao.Close();
+
+            return DataSetProduto;
+        }
+
+        public DataSet Listar_Grupo(string p_Produto_grupo)
+        {
+
+            SqlConnection Conexao = new SqlConnection(Chaveconexao);
+            DataSet DataSetProduto1 = new DataSet();
+            Conexao.Open();
+            string wQuery = "Select" +
+                          $"      p.Nome_Produto as Produto_grupo,          " +
+                          $"      g.Nome_Produto as Produto,                " +
+                          $"  from Produto p,                               " +
+                          $"      Produto g                                 " +
+                          $"      where p.id_produto = g.id_produto_grupo   " +
+                          $"      and g.Nome_produto = '{p_Produto_grupo}'  " +
+                          $"      group by p.Nome_Produto,                  " +
+                          $"                 g.Nome_Produto                 ";
+
+            foreach (DataRow linha in DataSetProduto1.Tables[0].Rows)
+            {
+                Console.WriteLine(linha["Produto_grupo"]);
+                Console.WriteLine(linha["Produto"]);
+            }
+            return DataSetProduto1;
+        }
+
+
+
+
+    }
+}
+
---
+++ pequisa_produto/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Produto
+{
+    class Program
+    {
+        static void Main()
+        {
+            Projeto_Lib.Class1 Pesquisar = new Projeto_Lib.Class1();
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("     Diga seu produto   ");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("     Como posso ajudar:       ");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("1- Para pesquisar pelo NOME");
+            Console.WriteLine("2- Para pesuisar pelo GRUPO");
+            Console.WriteLine("3- Para cadastrar o produto");
+            Console.WriteLine("4- Para alterar o produto");
+            Console.WriteLine("5- Para excluir o produto");
+            Console.WriteLine("6- Para sair");
+            string resposta = Console.ReadLine();
+
+            switch (resposta)
+            {
+                case "1":
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("     Digite o produto que queria encontrar   ");
+                    Console.WriteLine("------------------------------------------");
+                    string resposta1 = Console.ReadLine();
+
+                    Pesquisar.Listar_Produtos(resposta1);
+
+
+                    break;
+
+                case "2":
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("     Digite o grupo que queira encontar  ");
+                    Console.WriteLine("------------------------------------------");
+                    string resposta2 = Console.ReadLine();
+
+                    Pesquisar.Listar_Grupo(resposta2);
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+
+                case "5":
+
+                    break;
+
+                case "6":
+
+                    break;
+
+                default:
+
+                    break;
+            }
+
+
+
+        }
+
+    }
+}
